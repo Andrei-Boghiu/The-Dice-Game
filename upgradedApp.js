7d2ac3b232aa3c9d4ff2fe780ea2cc36dd3b1da5
Andrew <124816994+Andrei-Boghiu@users.noreply.github.com>
2023-05-01 13:07:06 +0300

    Update upgradedApp.js

--- upgradedApp.js
+++ upgradedApp.js
@@ -1,28 +1,30 @@
- // This is the js file I wrote after I watched the solution video from App Brewery Bootcamp.
- // This is not the same solution from App Brewery Bootcamp, in fact,  is pretty different from their solution.
- // I just used their solution as inspiration to create this file.
+// This is the js file I wrote after I watched the solution video from App Brewery Bootcamp.
+// This is not the same solution from App Brewery Bootcamp, in fact,  is pretty different from their solution.
+// I just used their solution as inspiration to create this file.
 
-// Create Random Scores for both players
-const randomScoreOne = Math.floor(Math.random() * 6 ) + 1;
-const randomScoreTwo = Math.floor(Math.random() * 6 ) + 1;
+// To make this script work, don't forget to uncomment the <img> and <script> tags from the index.html and comment the first <script> tag.
 
-console.log(randomScoreOne, randomScoreTwo);
+ // Create Random Scores for both players
+ const randomScoreOne = Math.floor(Math.random() * 6 ) + 1;
+ const randomScoreTwo = Math.floor(Math.random() * 6 ) + 1;
 
-// Display the right image for the random score
-    // For Player One
-    const dicePlayerOne = document.querySelectorAll("img")[0];
-    const srcAttributeOne = `./images/dice${randomScoreOne}.png`;
-    dicePlayerOne.setAttribute("src", srcAttributeOne);
-    // For Player Two
-    const dicePlayerTwo = document.querySelectorAll("img")[1];
-    const srcAttributeTwo = `./images/dice${randomScoreTwo}.png`;
-    dicePlayerTwo.setAttribute("src", srcAttributeTwo);
+ console.log(randomScoreOne, randomScoreTwo);
 
-// The logic of the winner
-if ( randomScoreOne === randomScoreTwo ) {
-    document.querySelector("h1").innerText = "🍺Draw!🍺"
-} else if ( randomScoreOne > randomScoreTwo ) {
-    document.querySelector("h1").innerText = "🍺 Salt Wins A Beer"
-} else if ( randomScoreOne < randomScoreTwo ) {
-    document.querySelector("h1").innerText = " Shadow Wins A Beer 🍺"
-} else { document.querySelector("h1").innerText = "There's an error, fix it!"}
+ // Display the right image for the random score
+     // For Player One
+     const dicePlayerOne = document.querySelectorAll("img")[0];
+     const srcAttributeOne = `./images/dice${randomScoreOne}.png`;
+     dicePlayerOne.setAttribute("src", srcAttributeOne);
+     // For Player Two
+     const dicePlayerTwo = document.querySelectorAll("img")[1];
+     const srcAttributeTwo = `./images/dice${randomScoreTwo}.png`;
+     dicePlayerTwo.setAttribute("src", srcAttributeTwo);
+
+ // The logic of the winner
+ if ( randomScoreOne === randomScoreTwo ) {
+     document.querySelector("h1").innerText = "🍺Draw!🍺"
+ } else if ( randomScoreOne > randomScoreTwo ) {
+     document.querySelector("h1").innerText = "🍺 Salt Wins A Beer"
+ } else if ( randomScoreOne < randomScoreTwo ) {
+     document.querySelector("h1").innerText = " Shadow Wins A Beer 🍺"
+ } else { document.querySelector("h1").innerText = "There's an error, fix it!"}
